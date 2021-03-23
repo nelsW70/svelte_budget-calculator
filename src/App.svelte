44bf0c28c1961456261976e6a571c33e2a4a31f6
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-23 10:51:16 -0400

    add: props and default props to title

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
 	import Navbar from './Navbar.svelte' 
+	import Title from './Title.svelte' 
 </script>
 
 <Navbar/>
+<Title title="add expense"/>
+<Title title="expense list"/>
+<Title />
+ 
