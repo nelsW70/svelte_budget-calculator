6e8f060ee8d56b8eb9ae444c43eec6e6ad7f20b3
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-24 11:04:53 -0400

    29: reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,15 @@
 	// components
 	import Navbar from "./Navbar.svelte";
 	import ExpensesList from "./ExpensesList.svelte";
+	import Totals from "./Totals.svelte";
 	// data
 	import expensesData from "./expenses";
 	// variables
 	let expenses = [...expensesData];
+	// reactive
+	$: total = expenses.reduce((acc, curr) => {
+		return (acc += curr.amount);
+	}, 0);
 	// functions
 	function removeExpense(id) {
 		expenses = expenses.filter((item) => item.id !== id);
@@ -20,9 +25,12 @@
 
 <Navbar />
 <main class="content">
+	<Totals title="total expenses" {total} />
 	<ExpensesList {expenses} />
 	<button
 		type="button"
 		class="btn btn-primary btn-block"
-		on:click={clearExpenses}>clear expenses</button>
+		on:click={clearExpenses}>
+		clear expenses
+	</button>
 </main>
